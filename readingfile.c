ad1e04536611d01b94ffc57fcdfb61f26ae6157f
Andrey <32015678+Andrey017@users.noreply.github.com>
2017-10-26 22:43:05 +0300

    Add files via upload

---
+++ readingfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct paket{
+    int i;
+    char m[256];
+    float f;
+};
+
+int main(int argc, char *argv[])
+{
+    long size;
+    struct paket p;
+    FILE *pFile;
+    pFile= fopen(argv[1], "rb");
+
+    /*The file name is not set*/
+    if (argc == 1){
+        printf("ERROR");
+    }
+
+    /*Open the file*/
+    while (fread(&p, sizeof(struct paket), 1, pFile)) {
+        printf("%d %s %f\n", p.i, p.m, p.f);
+    }
+
+    puts("\n");
+
+    /*File size*/
+    fseek(pFile, 0, SEEK_END);
+    size = ftell(pFile);
+    fseek(pFile, 0, SEEK_SET);
+    printf("File size: %ld", size);
+
+    fclose(pFile);
+
+    return 0;
+}
