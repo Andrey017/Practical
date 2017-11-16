1f2b54f71e794ab98bd2eda8ebbf679ef12bb23f
Andrey <32015678+Andrey017@users.noreply.github.com>
2017-11-16 21:14:41 +0300

    Update readingfile.c

--- readingfile.c
+++ readingfile.c
@@ -9,30 +9,30 @@ struct paket{
 
 int main(int argc, char *argv[])
 {
-    long size;
+    int size;
     struct paket p;
     FILE *pFile;
-    pFile= fopen(argv[1], "rb");
 
     /*The file name is not set*/
     if (argc == 1){
         printf("Enter the file name");
+        exit(1);
     }
 
     /*Open the file*/
     else{
 
+        pFile= fopen(argv[1], "rb");
+
         while (fread(&p, sizeof(struct paket), 1, pFile)) {
             printf("%d %s %f\n", p.i, p.m, p.f);
         }
-
-        puts("\n");
 
         /*File size*/
         fseek(pFile, 0, SEEK_END);
         size = ftell(pFile);
         fseek(pFile, 0, SEEK_SET);
-        printf("File size: %ld%s", size, "\tbytes");
+        printf("File size: %d%s", size, "\tbytes");
 
         fclose(pFile);
 
